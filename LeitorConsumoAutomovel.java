f0fba271f34337ac0beba4a9cc302688a4f8dba0
Joao Pedro Souza Silva <jpjoaopedro201444@gmail.com>
2024-04-18 16:55:20 -0300

    Correção

--- LeitorConsumoAutomovel.java
+++
@@ -1,9 +0,0 @@
-public class LeitorConsumoAutomovel {
-    public static double lerDistanciaPercorrida() {
-        return Prompt.lerDecimal("Digite a distância total percorrida pelo automóvel (em km): ");
-    }
-
-    public static double lerCombustivelGasto() {
-        return Prompt.lerDecimal("Digite o total de combustível gasto (em litros): ");
-    }
-}
